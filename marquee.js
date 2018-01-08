df3dad625d8fe5c5c9e7586d1f891fe2f7760cab
Thomas Hockaday <thomas.hockaday@opia-sp.com>
2018-01-08 09:32:47 +0000

    Add ability to toggle marquee when clicking extension icon again

--- marquee.js
+++ marquee.js
@@ -1,3 +1,7 @@
 (function() {
-  document.body.innerHTML = "<marquee>" + document.body.innerHTML;
+  if (document.body.innerHTML.indexOf("<marquee>") > -1) {
+    document.body.innerHTML = document.body.innerHTML.replace("<marquee>", "").replace("</marquee>", "");
+  } else {
+    document.body.innerHTML = "<marquee>" + document.body.innerHTML;
+  }
 })();
